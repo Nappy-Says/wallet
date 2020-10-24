5e703be3f2e1dc94fd6306a7622e5e1c24904a9f
Nappy <chokolatapapito@gmail.com>
2020-10-24 20:20:06 +0500

    edit main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"sync"
 	"time"
 )
 
 func main() {
-	anotherExample()
-	
+	data := make([]int, 1_000_000)
+	for _, i := range data {
+		data[i] = i
+	}
+
+	ch := make(chan int)
+	defer close(ch)
+	parts := 10
+	size := len(data) / parts
+
+	for i := 0; i < parts; i++ {
+		go func(ch chan<- int, data []int) {
+			sum := 0
+			for _, v := range data {
+				sum += v
+			}
+			ch <- sum
+		}(ch, data[i*size:(i+1)*size])
+	}
+	total := 0
+	for i := 0; i < parts; i++ {
+		total += <-ch
+	}
+	log.Print(total)
 }
 
 func sum() {
@@ -67,35 +88,5 @@ func tick(done <-chan struct{}) {
 		case <-time.After(time.Second):
 			log.Print("tick")
 		}
-	}
-}
-
-type Producer struct {
-	OutChan chan int
-}
-
-func (p *Producer) produce() {
-	for {
-		time.Sleep(2 * time.Second)
-		p.OutChan <- rand.Int()
-	}
-}
-func (p *Producer) getOutChan() <-chan int {
-	return p.OutChan
-}
-func anotherExample() {
-	p := Producer{OutChan: make(chan int, 10)}
-	go p.produce()
-	prodChan := p.getOutChan()
-	ticker := time.NewTicker(2 * time.Second)
-	for {
-		select {
-		case a := <-prodChan:
-			log.Println("got from a: ", a)
-		case s := <-ticker.C:
-			log.Println("got from s", s)
-
-		}
-	}
 	}
 }
